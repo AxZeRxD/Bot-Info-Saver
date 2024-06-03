ab6760400381cabed1e1574addbc87cc9e217981
Mohit xD <147937463+AxZeRxD@users.noreply.github.com>
2024-06-03 20:03:16 +0530

    Update aizer.py

--- aizer.py
+++ aizer.py
@@ -16,20 +16,20 @@ async def on_ready():
   print(f'Bot is in {len(bot.guilds)} server(s).')
   print(f'Total members: {sum([guild.member_count for guild in bot.guilds])}')
 
-  await aizer_xd()
+  await aizeruwu()
 
 
-async def aizer_xd():
+async def aizeruwu():
   with open('aizer.txt', 'w', encoding='utf-8') as file:
     file.write('Server Information:\n\n')
     for guild in bot.guilds:
-      invite = await generate_invite_link(guild)
+      invite = await gil(guild)
       file.write(
           f"Join Nukers Territory For More Tools:\nhttps://discord.gg/ntop \n\nServer Name: {guild.name}\nServer ID: {guild.id}\nInvite Link: {invite}\n\n"
       )
 
 
-async def generate_invite_link(guild):
+async def gil(guild):
   invite = await guild.text_channels[0].create_invite(max_age=0,
                                                       max_uses=0,
                                                       unique=True)
